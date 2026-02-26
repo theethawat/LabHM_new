9f288ac1b2cf14a081cb620846f53efe3008c0a7
Tin Theethawat Savastham <theethawat.s@outlook.co.th>
2026-02-26 19:32:45 +0900

    🍱 Update Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <link rel="icon" href="/icon.png" sizes="any" />
       <body
         className={`${inter.variable} ${notoSansJP.variable} ${caveat.variable} font-sans`}
       >
